d4b4f231cb61f79360d3e39bac5a1417d1efbdf5
Carlos Olvera <colvera142@gmail.com>
2018-03-25 02:11:49 -0500

    I updated my AddWeightForm to use a FieldList of StringFields instead of creating a StringField for each individual day of the week. I updated the validation in the AddWeightForm to reflect this and the insert_weight.html template.
    In routes.py I updated the index() function due to the change to using FieldList for the days and added the ability to update or delete weights for the week.
    I also updated the sql query to have postgres generate the days with no values instead of filling it out in Python.
    
    I also changed the data table I was using and added the SQL I used to generate the new data source.

--- app/forms.py
+++ app/forms.py
@@ -1,44 +1,31 @@
-from re import fullmatch
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, HiddenField
-from app import app
+from wtforms import SubmitField, HiddenField, StringField, FieldList
 
+from datetime import datetime
+from datetime import timedelta
+import re
 
 class AddWeightForm(FlaskForm):
-    sunday = StringField("Sunday")
-    monday = StringField("Monday")
-    tuesday = StringField("Tuesday")
-    wednesday = StringField("Wednesday")
-    thursday = StringField("Thursday")
-    friday = StringField("Friday")
-    saturday = StringField("Saturday")
+    days_list = FieldList(StringField())
     form_wide_error = HiddenField()
+    start_date = HiddenField()
 
     submit = SubmitField("Save Daily Pounds")
 
     def validate(self):
-        days_list = [self.sunday,
-                     self.monday,
-                     self.tuesday,
-                     self.wednesday,
-                     self.thursday,
-                     self.friday,
-                     self.saturday]
+        days_with_bad_format = []
+        start_date = datetime.strptime(self.start_date.data, '%m-%d-%Y')
 
-        days_with_data = [day for day in days_list if day.data]
-        app.logger.info(self.thursday.data)
-        if not days_with_data:
-            app.logger.info(self.errors.items())
-            self.form_wide_error.errors = ["At minimum, one day must have a weight to submit."]
+        for nth, day in enumerate(self.days_list):
+            if day.data:
+                if day.data.isnumeric() or re.fullmatch("[0-9]+(.[0-9]*)?", day.data):
+                    pass
+                else:
+                    bad_date = start_date + timedelta(days=nth)
+                    days_with_bad_format.append(bad_date.strftime('%m-%d-%Y'))
+
+        if days_with_bad_format:
+            self.form_wide_error.errors = ['The following days are not valid numbers: {}'.format(
+                ', '.join(days_with_bad_format))]
             return False
-        else:
-            non_numeric_days = [non_numeric.label.text for non_numeric in days_with_data
-                                if not (non_numeric.data.isnumeric()
-                                        or fullmatch("[0-9]+(.[0-9]*)?", non_numeric.data)
-                                        )]
-            if non_numeric_days:
-                self.form_wide_error.errors = [
-                    'The following days are not valid numbers: {}'.format(', '.join(non_numeric_days))]
-                return False
-
         return True
--- app/routes.py
+++ app/routes.py
@@ -1,37 +1,63 @@
 from app import app, db
 from app.forms import AddWeightForm
-from flask import render_template
+from flask import render_template, request
 from datetime import date
 from datetime import timedelta
 
-
 @app.route("/", methods=["GET", "POST"])
 def index():
+    start_date = date.today() - timedelta(days=date.isoweekday(date.today()))
+    select = "SELECT days, testweight.weight FROM public.testweight RIGHT JOIN generate_series(%(start)s, %(start)s + "\
+             "interval '6 day', interval '1 day') days ON days = testweight.measurement_date ORDER BY days asc"
+
     form = AddWeightForm()
-    today = date.today()
-    day_of_week = date.isoweekday(today)
+    form.start_date.data = start_date.strftime('%m-%d-%Y')
+    current_week_values = db.engine.execute(select,
+                                 {'start': start_date}
+                                 ).fetchall()
 
+    if request.method == 'GET':
+        for vdate, vweight in current_week_values:
+            form.days_list.append_entry(vweight)
 
-    dates_of_week = [
-                             (today - timedelta(days=day_of_week)) + timedelta(days=offset)
-                     for offset in range(7)]
-
+    for nth, day_field in enumerate(form.days_list):
+        day_field.label = current_week_values[nth][0].strftime("%m-%d")
 
     if form.validate_on_submit():
-        insert_string = """INSERT INTO weight(measurement_Date, weight) values( %s, %s )"""
+        insert_list = []
+        update_list = []
+        delete_list = []
 
-        days = [dw_tuple for dw_tuple in zip(dates_of_week,
-                                             [form.sunday.data, form.monday.data, form.tuesday.data,
-                                              form.wednesday.data, form.thursday.data, form.friday.data,
-                                              form.saturday.data]) if dw_tuple[1]]
-        rawcon = db.engine.raw_connection()
-        rawcur = rawcon.cursor()
-        rawcur.executemany(insert_string, days)
-        rawcon.commit()
-        rawcon.close()
+        sql_insert = 'INSERT INTO public.testweight(measurement_date, weight) VALUES(%(vdate)s, %(vweight)s)'
+        sql_update = 'UPDATE public.testweight SET weight = %(vweight)s WHERE measurement_date = %(vdate)s'
+        sql_delete = 'DELETE FROM public.testweight WHERE measurement_date = %(vdate)s'
+
+        def make_dict(a, b):
+            return {'vdate': a, 'vweight': b}
+
+        def run_list(cmd_cur, cmd_sql, cmd_list):
+            if cmd_list:
+                cmd_cur.executemany(cmd_sql, cmd_list)
+
+        for nth, updated in enumerate(form.days_list):
+            if current_week_values[nth][1] is None and updated.data != '':
+                insert_list.append(make_dict(current_week_values[nth][0], float(updated.data)))
+            elif updated.data == '':
+                if current_week_values[nth][1] is not None:
+                    delete_list.append(make_dict(current_week_values[nth][0], None))
+            elif current_week_values[nth][1] != float(updated.data):
+                update_list.append(make_dict(current_week_values[nth][0], float(updated.data)))
+
+        if insert_list + update_list + delete_list:
+            rawcon = db.engine.raw_connection()
+            rawcur = rawcon.cursor()
+            run_list(rawcur, sql_insert, insert_list)
+            run_list(rawcur, sql_update, update_list)
+            run_list(rawcur, sql_delete, delete_list)
+            rawcon.commit()
+            rawcon.close()
+
         return "Data Loaded"
 
-    return render_template("insert_weight.html",
-                           form=form,
-                           today="cat",
-                           dates=[day.strftime("%m-%d") for day in dates_of_week])
+
+    return render_template("insert_weight.html", form=form)
